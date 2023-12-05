e0798b9bbdf79c91140984c9616d545f71799908
Narsimha CHELLABOINA <narsimhac.in@mouritech.com>
2023-12-05 10:35:57 +0530

    db is changed

--- app/views.py
+++ app/views.py
@@ -25,7 +25,7 @@ from django.core.files.base import ContentFile
 
 
 myclient = pymongo.MongoClient("mongodb://localhost:27017/")
-mydb = myclient["narsimha"]
+mydb = myclient["narsimha1"]
 mycol1 = mydb['app_driver_entry']
 mycol2 = mydb['app_hospital']
 mycol3 = mydb['app_user_entry']
